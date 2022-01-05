e092955fe264fc5990df9b3360fcbb3738fce5ee
Abhishek Dubey <ad99526@gmail.com>
2022-01-06 05:09:42 +0530

    Generic types added

--- src/index.tsx
+++ src/index.tsx
@@ -2,47 +2,49 @@ import ReactDOM from "react-dom";
 import React, { FC } from "react";
 
 const data = [
-  { name: "Abhishek", job: "recently quit unifize", happiness: 0 },
-  { name: "Amarjit", job: "Still at Frontrow", happiness: 0 }
+  { alias: "James Bond", rollNo: "143E" },
+  { alias: "Agent Z", rollNo: "143E" }
+
+  // { name: "Abhishek", job: "recently quit unifize", happiness: 0 },
+  // { name: "Amarjit", job: "Still at Frontrow", happiness: 0 }
 ];
 
-interface TableProps {
-  columns: Array<"name" | "job" | "happiness">;
-  data: Array<{
-    name: string;
-    job: string;
-    happiness: number;
-  }>;
+interface TableProps<GenericData> {
+  columns: Array<keyof GenericData>;
+  data: GenericData[];
 }
 
-const Table: FC<TableProps> = ({ data, columns }) => (
-  <table>
-    <thead>
-      <tr>
-        {columns.map((c, i) => (
-          <th key={i}>{c}</th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {data.map(
-        (row: { name: string; job: string; happiness: number }, i: number) => (
-          <tr key={row.name}>
+function Table<AvoidShadowedGenericData>({
+  data,
+  columns
+}: TableProps<AvoidShadowedGenericData>) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {columns.map((c, i) => (
+            <th key={i}>{c}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, i) => (
+          <tr key={i}>
             {columns.map((c, i) => (
               <td key={i}>{row[c]}</td>
             ))}
           </tr>
-        )
-      )}
-    </tbody>
-  </table>
-);
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <h2>Start editing to see some magic happen!</h2>
-      <Table data={data} columns={["name", "happiness", "job"]} />
+      <Table data={data} columns={["alias", "rollNo"]} />
     </div>
   );
 }
